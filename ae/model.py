691e8e5087261519727cda193d471605dfc78e4f
Zhiwen Owen Jiang <ZhiwenOwenJiang@gmail.com>
2024-12-27 07:51:00 -0500

    init repo

---
+++ ae/model.py
@@ -0,0 +1,72 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+"""
+An autoencoder to efficient representation learning
+Encoder: 3D CNN of multiple layers and channels
+Decoder: a single fully connected layer
+
+1. Volumetric images: 3D CNN; surface-based images: GCN
+2. images of fixed shape 
+
+"""
+
+
+class Autoencoder(nn.Module):
+    def __init__(self, input_shape, latent_dim):
+        super().__init__()
+        self.input_shape = input_shape
+        self.latent_dim = latent_dim
+
+        self.batchnorm = nn.BatchNorm3d(1)
+
+        self.encoder = nn.Sequential(
+            self.encoder_block(1, 16),
+            self.encoder_block(16, 32),
+            self.encoder_block(32, 64),
+            self.encoder_block(64, 128),
+            self.encoder_block(128, 256)
+        )
+
+        with torch.no_grad():
+            x_dummy = torch.zeros(1, *input_shape) # Dummy input to get dimensions
+            encoded_dummy = self.encoder(x_dummy)
+            self.flattened_size = encoded_dummy.numel()
+        
+        self.fc_enc = nn.Linear(self.flattened_size, latent_dim)
+        flattened_image_size = input_shape[0] * input_shape[1] * input_shape[2] * input_shape[3]
+        self.output_layer = nn.Linear(latent_dim, flattened_image_size)
+
+        self._init_weights()
+
+    def encoder_block(self, input_channels, output_channels):
+        encoder = nn.Sequential(
+            nn.Conv3d(input_channels, output_channels, kernel_size=3, padding=1, bias=True),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv3d(output_channels, output_channels, kernel_size=3, padding=1, bias=True),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=2, stride=2)
+        )
+
+        return encoder
+
+    def forward(self, input_batch):
+        bn_output = self.batchnorm(input_batch)
+        encoded = self.encoder(bn_output)
+        encoded_flat = encoded.view(encoded.size(0), -1)
+        latent = self.fc_enc(encoded_flat)
+        output = self.output_layer(latent)
+        output = output.view(-1, *self.input_shape)
+
+        return output
+
+    def _init_weights(self):
+        for m in self.modules():
+            if type(m) in {nn.Linear, nn.Conv3d}:
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
+                if m.bias is not None:
+                    _, fan_out = nn.init._calculate_fan_in_and_fan_out(m.weight.data)
+                    bound = 1 / np.sqrt(fan_out)
+                    nn.init.normal_(m.bias, -bound, bound)
---
+++ ae/training.py
@@ -0,0 +1,84 @@
+import logging
+import datetime
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+    
+from .dsets import ImageDataset
+from .model import Autoencoder
+from heig.fpca import LocalLinear
+
+
+class TrainingApp:
+    def __init__(
+            self, 
+            input_shape, 
+            latent_dim,
+            epochs,
+            batch_size,
+            lr,
+            num_workers,
+        ):
+        # training parameters
+        self.input_shape = input_shape
+        self.latent_dim = latent_dim
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.lr = lr
+        self.num_workers = num_workers
+
+        self.use_cuda = torch.cuda.is_available()
+        if self.use_cuda:
+            self.batch_size *= torch.cuda.device_count()
+        self.device = torch.device("cuda" if self.use_cuda else "cpu")
+        self.model = self.initModel()
+        self.optimizer, _ = self.initOptimizer()
+        self.time_str = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
+        self.logger = logging.getLogger(__name__)
+
+    def initModel(self):
+        model = Autoencoder()
+        if self.use_cuda:
+            self.logger.info("Using CUDA; {} devices.".format(torch.cuda.device_count()))
+            if torch.cuda.device_count() > 1:
+                model = nn.DataParallel(model)
+            model = model.to(self.device)
+        return model
+
+    def initOptimizer(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+        lr_scheduler_config = {
+            "scheduler": ReduceLROnPlateau(
+                optimizer,
+                "min",
+                patience=4,
+                min_lr=self.hparams["lr"] / 1000,
+                factor=0.5,
+            ),
+            "interval": "epoch",
+            "frequency": 1,
+            "monitor": "val_loss",
+            "strict": True,
+            "name": None,
+        }
+
+        return optimizer, lr_scheduler_config
+
+    def initDl(self, is_train):
+        ds = ImageDataset(
+
+        )
+        dl = DataLoader(
+            ds,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.use_cuda,
+        )
+
+        return dl
+
+    def initTensorboardWriters(self):
+        pass
